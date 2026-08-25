f721fcb4f568d11172342ceebdf048fd4a556442
Umaiza <missumaiza15@gmail.com>
2026-08-25 20:58:21 +0800

    Level-0: Renaming the agent and introduction

---
+++ src/main/java/Judey.java
@@ -0,0 +1,21 @@
+public class Judey {
+    public static void main(String[] args) {
+        /*
+        String banner = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";*/
+        System.out.println("----------------------------------------");
+        String banner = "JJJJJJ  uu   uu  dddddd   eeeeeee  yy   yy\n" +
+                "   JJ   uu   uu  dd   dd  ee       yy   yy\n" +
+                "   JJ   uu   uu  dd   dd  eeeee     yyyyy\n" +
+                "JJ JJ   uu   uu  dd   dd  ee         yyy\n" +
+                " JJJ     uuuu u  dddddd   eeeeeee    yyy";
+        System.out.println(banner + "\n");
+        System.out.println("Hello! I'm Judey.\n" + "What can I do for you?");
+        System.out.println("----------------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("----------------------------------------");
+    }
+}
