9cab55debbfe288f033ebfb5707f2126dda2a797
Umaiza <missumaiza15@gmail.com>
2026-08-26 23:42:58 +0800

    Level-8: Date and time format

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,9 +1,32 @@
-public class Deadline extends Task {
-    protected String by;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
-    public Deadline(String desc, String by) {
+public class Deadline extends Task {
+    private static final DateTimeFormatter INPUT_FORMAT = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
+    private static final DateTimeFormatter OUTPUT_FORMAT = DateTimeFormatter.ofPattern("MMM dd yyyy, h:mma");
+
+    protected LocalDateTime by;
+
+    public Deadline(String desc, String by) throws JudeyException {
         super(desc);
-        this.by = by;
+        this.by = parseDateTime(by);
+    }
+
+    private LocalDateTime parseDateTime(String text) throws JudeyException {
+        try {
+            return LocalDateTime.parse(text);
+        } catch (DateTimeParseException e1) {
+            try {
+                return LocalDateTime.parse(text, INPUT_FORMAT);
+            } catch (DateTimeParseException e2) {
+                throw new JudeyException("Please use date format: d/M/yyyy HHmm (e.g., 2/12/2019 1800)");
+            }
+        }
+    }
+
+    public LocalDateTime getBy() {
+        return this.by;
     }
 
     /**
@@ -17,7 +40,7 @@ public class Deadline extends Task {
     }
 
     @Override
-    public String toString(){
-        return "[D]" + super.toString() + " (by: " + this.by + ")\n";
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + by.format(OUTPUT_FORMAT) + ")\n";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,11 +1,34 @@
-public class Event extends Task {
-    protected String from;
-    protected String to;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
-    public Event(String desc, String from, String to){
+public class Event extends Task {
+    private static final DateTimeFormatter INPUT_FORMAT = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
+    private static final DateTimeFormatter OUTPUT_FORMAT = DateTimeFormatter.ofPattern("MMM dd yyyy, h:mma");
+
+    protected LocalDateTime from;
+    protected LocalDateTime to;
+
+    public Event(String desc, String from, String to) throws JudeyException {
         super(desc);
-        this.from = from;
-        this.to = to;
+        this.from = parseDateTime(from);
+        this.to = parseDateTime(to);
+    }
+
+    private LocalDateTime parseDateTime(String text) throws JudeyException {
+        try {
+            return LocalDateTime.parse(text);
+        } catch (DateTimeParseException e1) {
+            try {
+                return LocalDateTime.parse(text, INPUT_FORMAT);
+            } catch (DateTimeParseException e2) {
+                throw new JudeyException("Please use date format: d/M/yyyy HHmm (e.g., 2/12/2019 1800)");
+            }
+        }
+    }
+
+    public LocalDateTime getFrom() {
+        return this.from;
     }
 
     /**
@@ -19,9 +42,9 @@ public class Event extends Task {
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         String display = "[E]" + super.toString();
-        display += " (from: " + this.from + " to: " + this.to + ")\n";
+        display += " (from: " + this.from.format(OUTPUT_FORMAT) + " to: " + this.to.format(OUTPUT_FORMAT) + ")\n";
         return display;
     }
 }
