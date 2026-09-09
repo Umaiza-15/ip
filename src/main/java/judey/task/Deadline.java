7e86b97eca7d24fb840d3b000c813a62692c4180
Umaiza <missumaiza15@gmail.com>
2026-09-09 21:06:46 +0800

    A-Assertions: Fix assumption on due date in Deadline class
    After the the due date is passed into the constructor of the Deadline
    class, it is assumed that the 'by' field is set to a value that is not
    null because the due date is parsed. If the due date were null, a
    JudeyException would be thrown.
    
    Using assertion will help us catch if the exception catches all
    instances of due dates being null or invalid. This ensures that after
    the due date is parsed, the 'by' field is not null.
    
    Let's add an assertion to check that the 'by' field is not null after
    parsing the due date in the Deadline class constructor.
    This will help us identify any potential issues during development and
    testing.

--- src/main/java/judey/task/Deadline.java
+++ src/main/java/judey/task/Deadline.java
@@ -24,6 +24,9 @@ public class Deadline extends Task {
     public Deadline(String desc, String by) throws JudeyException {
         super(desc);
         this.by = parseDateTime(by);
+        assert this.by != null
+                : "parseDateTime() either returns a valid LocalDateTime or throws JudeyException; it should never "
+                + "leave 'by' as null once this constructor completes successfully";
     }
 
     /**
